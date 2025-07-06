b5b6ceb6f2d3e34fb6422bd98b3ed1f903c3392f
LuciusL <lucius.lechner@porsche.digital>
2025-07-06 21:37:16 +0200

    Game funktioniert zu 87%

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,8 @@
 module org.example.catan {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-    requires annotations;
     requires static lombok;
-    requires transitive javafx.graphics;
 
     opens org.example.catan to javafx.fxml;
     exports org.example.catan;
